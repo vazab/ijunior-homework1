a0040ddd44c634ca40d1a7b47a3b82b736cbc887
Vasilii Zabolotskii <vazabsky@gmail.com>
2022-08-13 18:01:37 +0900

    homework finish 1 iteration

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using System.Collections;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private Enemy _enemy;
+    [SerializeField] private int _enemyCount;
+    [SerializeField] private float _timer;
+
     private Transform[] _points;
-    
 
     private void Start()
     {
@@ -13,7 +17,20 @@ public class EnemySpawner : MonoBehaviour
         {
             _points[i] = transform.GetChild(i);
         }
+
+        StartCoroutine(SpawnEnemies());
     }
 
+    private IEnumerator SpawnEnemies()
+    {
+        WaitForSeconds waitForSeconds = new WaitForSeconds(_timer);
 
+        for (int i = 0; i < _enemyCount; i++)
+        {
+            int index = Random.Range(0, transform.childCount);
+            Instantiate(_enemy, _points[index].position, Quaternion.identity);
+
+            yield return waitForSeconds;
+        }
+    }
 }
